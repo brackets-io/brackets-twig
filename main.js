dd2e221964eed82465ee052a1df3fb792f8b6a4a
Athorcis <athorcis@yahoo.fr>
2016-07-22 22:15:12 +0200

    remove twig from file extension since htmlmixed mode already declare it

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ define(function (require) {
     LanguageManager.defineLanguage("twigmixed", {
         name: "Twig",
         mode: "twigmixed",
-        fileExtensions: ["twig", "html.twig", "twig.html"]
+        fileExtensions: ["html.twig", "twig.html"]
     })
     .done(function (twig) {
         twig._setLanguageForMode("twig:inner", twig);
